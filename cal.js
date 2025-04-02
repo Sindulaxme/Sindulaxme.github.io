cd8055b6f80afedfe2f83ca0a8bad0c21cdabb66
Sindulaxme <sindulaxme@gmail.com>
2025-04-02 21:54:52 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,7 @@
+// Tic-Tac-Toe Game
+const ticTacToe = document.getElementById("tic-tac-toe");
+ticTacToe.innerHTML = "<p>Coming Soon: Interactive Tic-Tac-Toe Game</p>";
+
+// Calculator Demo
+const calculator = document.getElementById("calculator");
+calculator.innerHTML = "<p>Coming Soon: Functional Calculator</p>";
